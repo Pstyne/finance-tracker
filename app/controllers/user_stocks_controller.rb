1fa235fa6e7085eb98aef30d4d9de2f4816f3647
Andrew Birt <abirt1986@gmail.com>
2020-10-29 19:47:47 -0400

    add functionality to add and remove stocks from portfolio

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,20 @@
 class UserStocksController < ApplicationController
+  def create
+    stock = Stock.check_db(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      stock.save
+    end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+    redirect_to my_portfolio_path
+  end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.find_by(user_id: current_user.id, stock_id: stock.id)
+    user_stock.destroy
+    flash[:notice] = "#{stock.name} (#{stock.ticker}) was successfully removed from portfolio"
+    redirect_to my_portfolio_path
+  end
 end
