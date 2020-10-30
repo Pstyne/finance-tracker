da9932661dd42df8473ae5a4c40e33de97de2691
Andrew Birt <abirt1986@gmail.com>
2020-10-30 00:58:17 -0400

    implement search feature to not include current user in search results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,14 @@ class UsersController < ApplicationController
 
   def search
     if params[:q].present?
-      @friend = params[:q]
-      if @friend
+      @friends = User.search(params[:q])
+      if @friends.any?
         respond_to do |format|
           format.js { render partial: "users/friend_result" }
         end
       else
         respond_to do |format|
-          flash.now[:alert] = "Counldn't find user"
+          flash.now[:alert] = "Couldn't find user"
           format.js { render partial: "users/friend_result" }
         end
       end
