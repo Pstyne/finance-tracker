e45fc07f1ae7335269038c3d23d4085f34d8a3a1
Andrew Birt <abirt1986@gmail.com>
2020-10-30 01:56:34 -0400

    implement friendship create functionality and use in view

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: params[:friend])
+    if current_user.save
+      flash[:notice] = "You are now following #{friend.first_name}"
+    else
+      flash[:alert] = "Something went wrong"
+    end
+    redirect_to friends_path
   end
 
   def destroy
